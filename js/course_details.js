14768e2cdc4bb83b4e5a616198a7b0b4e567caf6
Kirito <1737466486@qq.com>
2018-05-11 19:55:11 +0800

    Signed-off-by: Kirito <1737466486@qq.com>

--- js/course_details.js
+++ js/course_details.js
@@ -97,9 +97,35 @@ $(function(){
 			    signature: data.config.signature,// 必填，签名
 			    jsApiList: data.config.jsApiList // 必填，需要使用的JS接口列表
 			});
+			$('.course_pay').css({'display':'block'})
+			
 		 //通过ready接口处理成功验证
 			    wx.ready(function(){
 			    // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+			
+				  $('.course_pay div b').eq(1).click(function(){
+						
+						
+						wx.chooseWXPay({
+			
+						timestamp: data.pay_config.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+						
+						nonceStr: data.pay_config.nonceStr, // 支付签名随机串，不长于 32 位
+						
+						package: data.pay_config.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+						
+						signType: data.pay_config.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+						
+						paySign: data.pay_config.paySign, // 支付签名
+						
+						success: function (res) {
+						// 支付成功后的回调函数
+						console.log(res)
+							}
+				    	})
+		
+					})
+			
 			
 			    });
 			    //通过error接口处理失败验证
@@ -116,33 +142,10 @@ $(function(){
 				    }
 				});
 	    	
-	            $('.course_pay').css({'display':'block'})
+	            
 	    	 
 			    
-			    $('.course_pay div b').eq(1).click(function(){
-					
-					
-					wx.chooseWXPay({
-		
-					timestamp: data.pay_config.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-					
-					nonceStr: data.pay_config.nonceStr, // 支付签名随机串，不长于 32 位
-					
-					package: data.pay_config.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-					
-					signType: data.pay_config.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-					
-					paySign: data.pay_config.paySign, // 支付签名
-					
-					success: function (res) {
-					// 支付成功后的回调函数
-					console.log(res)
-				}
-	    	})
-					
-					
-					
-				})
+			  
 			    
 			    
 			   
