98a1728e872b4ed1acad8b3ec9890cb4ab68d907
Kirito <1737466486@qq.com>
2018-05-28 17:58:27 +0800

    Signed-off-by: Kirito <1737466486@qq.com>

--- js/group_pay.js
+++ js/group_pay.js
@@ -43,6 +43,82 @@ $(function(){
 			$('.v_det .v_det_s2').html('课程时长：' + data.last_days + '天')
 			$('.v_det .v_det_s3').html('购买截止时间：' + data.alloc_at.substr(0, 10))
 			$('.group_foot p').eq(1).find('span').html('<div><s>￥'+data.price/100+'</s>&nbsp￥'+data.groupon_price/100+'</div><b>我要参团</b>')
+		    //微信配置
+	        //优惠金额
+	        var discount_amount=data.price/100-data.groupon_price/100
+	
+			$.post("http://api.speaka.cn/api/wxconfig", {
+				location: window.location.href
+			},
+			function(data) {
+				console.log(data)
+					if(data.status == 1) {
+					//微信支付
+					//通过config接口注入权限验证配置
+					wx.config({
+						debug: data.config.debug, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+						appId: data.config.appId, // 必填，公众号的唯一标识
+						timestamp: data.config.timestamp, // 必填，生成签名的时间戳
+						nonceStr: data.config.nonceStr, // 必填，生成签名的随机串
+						signature: data.config.signature, // 必填，签名
+						jsApiList: data.config.jsApiList // 必填，需要使用的JS接口列表
+					});
+					//通过ready接口处理成功验证
+					wx.ready(function() {
+						// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+				        wx.onMenuShareAppMessage({    
+						    title: '参团立减'+discount_amount+'元！超有趣的少儿互动英文课！', // 分享标题    
+						    desc: 'Youtube英文教育红人家庭中国首秀，台湾帅气老师Lyle担当讲解。欢乐体验美国地道家庭生活', // 分享描述    
+						    link: group_url, // 分享链接    
+						    imgUrl: 'http://s.speaka.cn/static/logo-white.png', // 分享图标    
+						    type: '', // 分享类型,music、video或link，不填默认为link    
+						    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空    
+						    success: function () {     
+						        // 用户确认分享后执行的回调函数    
+						        window.location.href = 'http://h5.speaka.cn/front/html/course_details.html?'+commodity_id
+						    },    
+						    cancel: function () {     
+						        // 用户取消分享后执行的回调函数    
+						        window.location.href = 'http://h5.speaka.cn/front/html/course_details.html?'+commodity_id
+						    }    
+						});
+						wx.onMenuShareTimeline({    
+						    title: '参团立减'+discount_amount+'元！超有趣的少儿互动英文课！', // 分享标题    
+						    link: group_url, // 分享链接    
+						    imgUrl: 'http://s.speaka.cn/static/logo-white.png', // 分享图标    
+						    success: function () {     
+						        // 用户确认分享后执行的回调函数    
+						        window.location.href = 'http://h5.speaka.cn/front/html/course_details.html?'+commodity_id
+						    },    
+						    cancel: function () {     
+						        // 用户取消分享后执行的回调函数    
+						        window.location.href = 'http://h5.speaka.cn/front/html/course_details.html?'+commodity_id
+						    }    
+						});					
+		 
+					});
+					//通过error接口处理失败验证
+					wx.error(function(res) {
+						alert(res)
+						// config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+			
+					});
+					//判断当前客户端版本是否支持指定JS接口
+					wx.checkJsApi({
+						jsApiList: ['chooseImage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+						success: function(res) {
+							// 以键值对的形式返回，可用的api值true，不可用为false
+							// 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+						}
+					});
+			
+				} else {
+					alert('验证信息已失效，请重新获取订单信息！')
+				}
+				
+			},'json');
+
+
 
 		},
 		error:function(error){
@@ -238,78 +314,7 @@ $(function(){
 			console.log(error)
 		}
 	});
-		//微信配置
-	
-	$.post("http://api.speaka.cn/api/wxconfig", {
-		location: window.location.href
-	},
-	function(data) {
-		console.log(data)
-			if(data.status == 1) {
-			//微信支付
-			//通过config接口注入权限验证配置
-			wx.config({
-				debug: data.config.debug, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-				appId: data.config.appId, // 必填，公众号的唯一标识
-				timestamp: data.config.timestamp, // 必填，生成签名的时间戳
-				nonceStr: data.config.nonceStr, // 必填，生成签名的随机串
-				signature: data.config.signature, // 必填，签名
-				jsApiList: data.config.jsApiList // 必填，需要使用的JS接口列表
-			});
-			//通过ready接口处理成功验证
-			wx.ready(function() {
-				// config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-		        wx.onMenuShareAppMessage({    
-				    title: '参团立享优惠！超有趣的少儿互动英文课！', // 分享标题    
-				    desc: 'Youtube英文教育红人家庭中国首秀，台湾帅气老师Lyle担当讲解。欢乐体验美国地道家庭生活', // 分享描述    
-				    link: group_url, // 分享链接    
-				    imgUrl: 'http://s.speaka.cn/static/logo-white.png', // 分享图标    
-				    type: '', // 分享类型,music、video或link，不填默认为link    
-				    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空    
-				    success: function () {     
-				        // 用户确认分享后执行的回调函数    
-				        window.location.href = 'http://h5.speaka.cn/front/html/course_details.html?'+commodity_id
-				    },    
-				    cancel: function () {     
-				        // 用户取消分享后执行的回调函数    
-				        window.location.href = 'http://h5.speaka.cn/front/html/course_details.html?'+commodity_id
-				    }    
-				});
-				wx.onMenuShareTimeline({    
-				    title: '参团立享优惠！超有趣的少儿互动英文课！', // 分享标题    
-				    link: group_url, // 分享链接    
-				    imgUrl: 'http://s.speaka.cn/static/logo-white.png', // 分享图标    
-				    success: function () {     
-				        // 用户确认分享后执行的回调函数    
-				        window.location.href = 'http://h5.speaka.cn/front/html/course_details.html?'+commodity_id
-				    },    
-				    cancel: function () {     
-				        // 用户取消分享后执行的回调函数    
-				        window.location.href = 'http://h5.speaka.cn/front/html/course_details.html?'+commodity_id
-				    }    
-				});					
- 
-			});
-			//通过error接口处理失败验证
-			wx.error(function(res) {
-				alert(res)
-				// config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-	
-			});
-			//判断当前客户端版本是否支持指定JS接口
-			wx.checkJsApi({
-				jsApiList: ['chooseImage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-				success: function(res) {
-					// 以键值对的形式返回，可用的api值true，不可用为false
-					// 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-				}
-			});
-	
-		} else {
-			alert('验证信息已失效，请重新获取订单信息！')
-		}
-		
-	},'json');
+
 	
 	
 	
