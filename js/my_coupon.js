e17c8fe8bd1f4786664f402e8f5b312902ecc734
Kirito <1737466486@qq.com>
2018-06-06 17:29:55 +0800

    订单页优化

--- js/my_coupon.js
+++ js/my_coupon.js
@@ -1,49 +1,30 @@
-$(function(){
-       window.get_token=get_token;
-       var token = null;
-       function get_token(_results) {
-           token = 'Bearer ' + _results;
-        $.ajax({
-            type:'GET',
-            dataType:'JSON',
-            async:true,
-           //url:"../json/my_coupon.json",
-	        url:'http://api.speaka.cn/api/u/orders',
-	         beforeSend: function (request) {
-	             request.setRequestHeader("Authorization", token);
-	         },
-            success:function (data){
-                console.log("成功获取数据",data.info);
-                for(let i=0;i<data.info.length;i++){
-                    console.log(data.info[i]);
-                    $('.coupon').append(`
-	                <li>
-					<div class="coupon_left">
-						<div class="coupon_price">
-							<strong>${data.info[i].price}</strong>
-							<span>元</span>
-						</div>
-						<div class="use_time">
-							<span>有效期 :</span>
-							<span>
-${data.info[i].usable_start_at.substr(0,4)+"年"+data.info[i].usable_start_at.substr(5,2)+"月"+data.info[i].usable_start_at.substr(8,2)+"日"}
-</span>
-							<span>-</span>
-							<span>${data.info[i].usable_end_at.substr(0,4)+"年"+data.info[i].usable_end_at.substr(5,2)+"月"+data.info[i].usable_end_at.substr(8,2)+"日"}</span>
-						</div>
-					</div>
-					<div class="coupon_right">
-						<div>满减券</div>
-						<div>满&nbsp;<span>${data.info[i].required_price}</span>&nbsp;可用</div>
-					</div>
-				</li>
-	               `);
+'use strict';
 
-                }
-            },
-            error:function(res){
-                console.log(res)
-            }
-        })
-     }
+$(function() {
+	window.get_token = get_token;
+	var token = null;
+get_token()
+	function get_token(_results) {
+		token = 'Bearer ' + _results;
+		$.ajax({
+			type: 'get',
+			dataType: 'JSON',
+			async: true,
+			url: "../json/my_coupon.json",
+			//url:'api.speaka.cn/api/coupon/m',
+			beforeSend: function beforeSend(request) {
+				request.setRequestHeader("Authorization", token);
+			},
+			success: function success(data) {
+				console.log("成功获取数据", data.info);
+				for(var i = 0; i < data.info.length; i++) {
+					console.log(data.info[i]);
+					$('.coupon').append('\n\t                <li>\n\t\t\t\t\t<div class="coupon_left">\n\t\t\t\t\t\t<div class="coupon_price">\n\t\t\t\t\t\t\t<strong>' + data.info[i].price + '</strong>\n\t\t\t\t\t\t\t<span>\u5143</span>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class="use_time">\n\t\t\t\t\t\t\t<span>\u6709\u6548\u671F :</span>\n\t\t\t\t\t\t\t<span>\n' + (data.info[i].usable_start_at.substr(0, 4) + "年" + data.info[i].usable_start_at.substr(5, 2) + "月" + data.info[i].usable_start_at.substr(8, 2) + "日") + '\n</span>\n\t\t\t\t\t\t\t<span>-</span>\n\t\t\t\t\t\t\t<span>' + (data.info[i].usable_end_at.substr(0, 4) + "年" + data.info[i].usable_end_at.substr(5, 2) + "月" + data.info[i].usable_end_at.substr(8, 2) + "日") + '</span>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class="coupon_right">\n\t\t\t\t\t\t<div>\u6EE1\u51CF\u5238</div>\n\t\t\t\t\t\t<div>\u6EE1&nbsp;<span>' + data.info[i].required_price + '</span>&nbsp;\u53EF\u7528</div>\n\t\t\t\t\t</div>\n\t\t\t\t</li>\n\t               ');
+				}
+			},
+			error: function error(res) {
+				console.log(res);
+			}
+		});
+	}
 });
--- js/my_order.js
+++ js/my_order.js
@@ -1,60 +1,98 @@
-$(function(){
-	window.get_token=get_token;
+'use strict';
+
+$(function () {
+	window.get_token = get_token;
 	var token = null;
+	//get_token()
 	function get_token(_results) {
 		//console.log(_results)
 		token = 'Bearer ' + _results;
-	    $.ajax({
-	       type:'POST',
-	       dataType:'JSON',
-	       async:true,
-	       //url:"../json/my_order.json",
-            url:'http://api.speaka.cn/api/u/orders',
-	       beforeSend: function (request) {
-		        request.setRequestHeader("Authorization", token);
-		    },
-	       success:function (data) {
-	           console.log("成功获取数据",data.info);
-	           for(let i=0;i<data.info.length;i++){
-	               console.log(data.info[i].order_no);
-	               $('.orders').append(`
-	                <li>
-	                    <p>
-	                        <span>订单编号：</span>
-	                        <span class="order_no">${data.info[i].order_no}</span>
-	                    </p>
-	                    <p>
-	                        <span>商品名称：</span>
-	                        <span class="order_name">${data.info[i].name}</span>
-	                    </p>
-	                    <p>
-	                        <span>下单时间：</span>
-	                        <span class="order_time">${data.info[i].created_at}</span>
-	                    </p>
-	                    <p>
-	                        <span>价<span class="zhanwei">占位</span>格：</span>
-	                        <span class="price"><em>￥</em>${data.info[i].price}</span>  
-	                        <img class="group" src="../img/group.png" alt="">
-	                    </p>
-	                    <img  class="mark" src="" alt="">
-	                </li>
-	               `);
-	               if(data.info[i].state===1){
-	                   $('.mark').eq(i).attr('src','../img/pay_yes.png')
-	               }else{
-	                   $('.mark').eq(i).attr('src','../img/pay_no.png')
-	               }
-	               if(data.info[i].type_id===0){
-						$(".group").eq(i).hide()
-				   }else{
-                       $(".group").eq(i).show()
-				   }
-	           }
-	       },
-	       error:function(res){
-	       	alert(res)
-	       }
-	    })
-    }
+		$.ajax({
+			type: 'get',
+			dataType: 'JSON',
+			async: true,
+			//url:"../json/my_order.json",
+			url: 'http://api.speaka.cn/api/u/orders',
+			beforeSend: function beforeSend(request) {
+				request.setRequestHeader("Authorization", token);
+			},
+			success: function success(data) {
+				//console.log("成功获取数据",data.info);
+				for (var i = 0; i < data.info.length; i++) {
+					//console.log(data.info[i].order_no);
+					if (data.info[i].type_id === 0) {
+						$('.single_orders').append('\n\t\t                <li>\n\t\t                    <p>\n\t\t                        <span>\u8BA2\u5355\u7F16\u53F7\uFF1A</span>\n\t\t                        <span class="order_no">' + data.info[i].order_no + '</span>\n\t\t                        <em class=\'mark\'></em>\n\t\t                    </p>\n\t\t                    <p class=\'xuxian\'></p>\n\t\t                    <p>\n\t\t                        <span>\u5546\u54C1\u540D\u79F0\uFF1A</span>\n\t\t                        <span class="order_name">' + data.info[i].name + '</span>\n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u4E0B\u5355\u65F6\u95F4\uFF1A</span>\n\t\t                        <span class="order_time">' + data.info[i].created_at + '</span>\n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u8BA2\u5355\u91D1\u989D\uFF1A</span>\n\t\t                        <span class="price"><em>\uFFE5</em>' + data.info[i].price + '</span>  \n\t\t                    </p>\n\t\t                </li>\n\t\t               ');
+					}
+					if (data.info[i].state === 1) {
+						$('.mark').eq(i).html('已支付').css({ 'color': '#2FBBA9' });
+					}
+					if (data.info[i].state === 0) {
+						$('.mark').eq(i).html('未支付').css({ 'color': '#999999' });
+					}
+				}
+			},
+			error: function error(res) {
+				console.log(res);
+			}
+		});
+		//团购订单
+		$.ajax({
+			type: 'get',
+			dataType: 'JSON',
+			async: true,
+			//url:"../json/my_order.json",
+			url: 'http://api.speaka.cn/api/u/orders',
+			beforeSend: function beforeSend(request) {
+				request.setRequestHeader("Authorization", token);
+			},
+			success: function success(data) {
+				//console.log("成功获取数据",data.info);
+				for (var i = 0; i < data.info.length; i++) {
+					//console.log(data.info[i].order_no);
+					if (data.info[i].type_id === 1) {
+						if (data.info[i].alloc_state === 0) {
+							$('.group_orders').append('\n\t\t                <li>\n\t\t                    <p>\n\t\t                        <span>\u8BA2\u5355\u7F16\u53F7\uFF1A</span>\n\t\t                        <span class="order_no">' + data.info[i].order_no + '</span>\n\t\t                        <em class=\'mark\'></em>\n\t\t                    </p>\n\t\t                    <p class=\'xuxian\'></p>\n\t\t                    <p>\n\t\t                        <span>\u5546\u54C1\u540D\u79F0\uFF1A</span>\n\t\t                        <span class="order_name">' + data.info[i].name + '</span>\n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u4E0B\u5355\u65F6\u95F4\uFF1A</span>\n\t\t                        <span class="order_time">' + data.info[i].created_at + '</span>\n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u8BA2\u5355\u91D1\u989D\uFF1A</span>\n\t\t                        <span class="price"><em>\uFFE5</em>' + data.info[i].price + '</span>  \n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u6210\u56E2\u72B6\u6001\uFF1A</span>\n\t\t                        <span class="group_state">\u672A\u6210\u56E2</span>  \n\t\t                    </p>\n\t\t                     <p class=\'share_order\'>\n\t\t                        <span>\u56E2\u8D2D\u8BA2\u5355</span>\n\t\t                        <b commodity_id=\'' + data.info[i].commodity_id + '\'>\u9080\u8BF7\u597D\u53CB\u53C2\u56E2</b> \n\t\t                    </p>\n\t\t                </li>\n\t\t               ');
+						} else {
+							$('.group_orders').append('\n\t\t                <li>\n\t\t                    <p>\n\t\t                        <span>\u8BA2\u5355\u7F16\u53F7\uFF1A</span>\n\t\t                        <span class="order_no">' + data.info[i].order_no + '</span>\n\t\t                        <em class=\'mark\'></em>\n\t\t                    </p>\n\t\t                    <p class=\'xuxian\'></p>\n\t\t                    <p>\n\t\t                        <span>\u5546\u54C1\u540D\u79F0\uFF1A</span>\n\t\t                        <span class="order_name">' + data.info[i].name + '</span>\n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u4E0B\u5355\u65F6\u95F4\uFF1A</span>\n\t\t                        <span class="order_time">' + data.info[i].created_at + '</span>\n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u8BA2\u5355\u91D1\u989D\uFF1A</span>\n\t\t                        <span class="price"><em>\uFFE5</em>' + data.info[i].price + '</span>  \n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u6210\u56E2\u72B6\u6001\uFF1A</span>\n\t\t                        <span class="group_state" >\u5DF2\u6210\u56E2</span>  \n\t\t                    </p>\n\t\t                    <p class=\'share_order\'>\n\t\t                        <span>\u56E2\u8D2D\u8BA2\u5355</span>\n\t\t                        <b commodity_id=\'' + data.info[i].commodity_id + '\'>\u5206\u4EAB</b> \n\t\t                    </p>\n\t\t                </li>\n\t\t               ');
+						}
+					}
+					$('.group_state').css({ 'color': '#2FBBA9' });
+					if (data.info[i].state === 1) {
+						$('.mark').eq(i).html('已支付').css({ 'color': '#2FBBA9' });
+					}
+					if (data.info[i].state === 0) {
+						$('.mark').eq(i).html('未支付').css({ 'color': '#999999' });
+					}
+				}
+				$('.share_order b').click(function () {
+					var order_no = $(this).parent().parent().find('p').eq(0).children('span').eq(1).html();
+					var obj = {};
+					obj.title = '超有趣的少儿互动英文课！';
+					obj.desc = 'Youtube英文教育红人家庭中国首秀，台湾帅气老师Lyle担当讲解。欢乐体验美国地道家庭生活';
+					obj.share_url = 'http://h5.speaka.cn/front/html/group_pay.html?commodity_id=' + $(this).attr('commodity_id') + '&order_no=' + order_no;
+					console.log(obj);
+					if (window.webkit) {
+						window.webkit.messageHandlers.itemClick.postMessage(JSON.stringify(obj));
+					} else {
+						curson.punchCurson(JSON.stringify(obj));
+					}
+				});
+			},
+			error: function error(res) {
+				console.log(res);
+			}
+		});
+	}
+	$('.order_nav p').eq(1).click(function () {
+		$('.order_nav p').eq(1).find('b').css({ 'display': 'block' });
+		$('.group_orders').css({ 'display': 'block' });
+		$('.order_nav p').eq(0).find('b').css({ 'display': 'none' });
+		$('.single_orders').css({ 'display': 'none' });
+	});
+	$('.order_nav p').eq(0).click(function () {
+		$('.order_nav p').eq(1).find('b').css({ 'display': 'none' });
+		$('.group_orders').css({ 'display': 'none' });
+		$('.order_nav p').eq(0).find('b').css({ 'display': 'block' });
+		$('.single_orders').css({ 'display': 'block' });
+	});
 });
-
