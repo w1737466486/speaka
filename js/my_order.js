0ae500d0764b4df94e85deac4bd1364efc34a9c6
kirito <1737466486@qq.com>
2018-06-07 15:20:02 +0800

    订单页优化

--- js/my_order.js
+++ js/my_order.js
@@ -20,10 +20,10 @@ $(function () {
 				//console.log("成功获取数据",data.info);
 				for (var i = 0; i < data.info.length; i++) {
 					//console.log(data.info[i].order_no);
-					if (data.info[i].type_id === 0) {
+					if (data.info[i].type_id == 0) {
 						$('.single_orders').append('\n\t\t                <li>\n\t\t                    <p>\n\t\t                        <span>\u8BA2\u5355\u7F16\u53F7\uFF1A</span>\n\t\t                        <span class="order_no">' + data.info[i].order_no + '</span>\n\t\t                        <em class=\'mark\'></em>\n\t\t                    </p>\n\t\t                    <p class=\'xuxian\'></p>\n\t\t                    <p>\n\t\t                        <span>\u5546\u54C1\u540D\u79F0\uFF1A</span>\n\t\t                        <span class="order_name">' + data.info[i].name + '</span>\n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u4E0B\u5355\u65F6\u95F4\uFF1A</span>\n\t\t                        <span class="order_time">' + data.info[i].created_at + '</span>\n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u8BA2\u5355\u91D1\u989D\uFF1A</span>\n\t\t                        <span class="price"><em>\uFFE5</em>' + data.info[i].price + '</span>  \n\t\t                    </p>\n\t\t                </li>\n\t\t               ');
 					}
-					if (data.info[i].state === 1) {
+					if (data.info[i].state == 1) {
 						$('.mark').eq(i).html('已支付').css({ 'color': '#2FBBA9' });
 					}
 					if (data.info[i].state === 0) {
@@ -33,21 +33,21 @@ $(function () {
 				//团购订单
 				for (var i = 0; i < data.info.length; i++) {
 					//console.log(data.info[i].order_no);
-					if (data.info[i].type_id === 1) {
-						if (data.info[i].alloc_state === 0) {
+					if (data.info[i].type_id == 1) {
+						if (data.info[i].is_group_completed == 0) {
 							$('.group_orders').append('\n\t\t                <li>\n\t\t                    <p>\n\t\t                        <span>\u8BA2\u5355\u7F16\u53F7\uFF1A</span>\n\t\t                        <span class="order_no">' + data.info[i].order_no + '</span>\n\t\t                        <em class=\'mark\'></em>\n\t\t                    </p>\n\t\t                    <p class=\'xuxian\'></p>\n\t\t                    <p>\n\t\t                        <span>\u5546\u54C1\u540D\u79F0\uFF1A</span>\n\t\t                        <span class="order_name">' + data.info[i].name + '</span>\n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u4E0B\u5355\u65F6\u95F4\uFF1A</span>\n\t\t                        <span class="order_time">' + data.info[i].created_at + '</span>\n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u8BA2\u5355\u91D1\u989D\uFF1A</span>\n\t\t                        <span class="price"><em>\uFFE5</em>' + data.info[i].price + '</span>  \n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u6210\u56E2\u72B6\u6001\uFF1A</span>\n\t\t                        <span class="group_state">\u672A\u6210\u56E2</span>  \n\t\t                    </p>\n\t\t                     <p class=\'share_order\'>\n\t\t                        <span>\u56E2\u8D2D\u8BA2\u5355</span>\n\t\t                        <b commodity_id=\'' + data.info[i].commodity_id + '\'>\u9080\u8BF7\u597D\u53CB\u53C2\u56E2</b> \n\t\t                    </p>\n\t\t                </li>\n\t\t               ');
 						} else {
 							$('.group_orders').append('\n\t\t                <li>\n\t\t                    <p>\n\t\t                        <span>\u8BA2\u5355\u7F16\u53F7\uFF1A</span>\n\t\t                        <span class="order_no">' + data.info[i].order_no + '</span>\n\t\t                        <em class=\'mark\'></em>\n\t\t                    </p>\n\t\t                    <p class=\'xuxian\'></p>\n\t\t                    <p>\n\t\t                        <span>\u5546\u54C1\u540D\u79F0\uFF1A</span>\n\t\t                        <span class="order_name">' + data.info[i].name + '</span>\n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u4E0B\u5355\u65F6\u95F4\uFF1A</span>\n\t\t                        <span class="order_time">' + data.info[i].created_at + '</span>\n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u8BA2\u5355\u91D1\u989D\uFF1A</span>\n\t\t                        <span class="price"><em>\uFFE5</em>' + data.info[i].price + '</span>  \n\t\t                    </p>\n\t\t                    <p>\n\t\t                        <span>\u6210\u56E2\u72B6\u6001\uFF1A</span>\n\t\t                        <span class="group_state" >\u5DF2\u6210\u56E2</span>  \n\t\t                    </p>\n\t\t                    <p class=\'share_order\'>\n\t\t                        <span>\u56E2\u8D2D\u8BA2\u5355</span>\n\t\t                        <b commodity_id=\'' + data.info[i].commodity_id + '\'>\u5206\u4EAB</b> \n\t\t                    </p>\n\t\t                </li>\n\t\t               ');
 						}
 					}
 					$('.group_state').css({ 'color': '#2FBBA9' });
-					if (data.info[i].state === 1) {
+					if (data.info[i].state == 1) {
 						$('.mark').eq(i).html('已支付').css({ 'color': '#2FBBA9' });
 					}
-					if (data.info[i].state === 0) {
+					if (data.info[i].state == 0) {
 						$('.mark').eq(i).html('未支付').css({ 'color': '#999999' });
 					}
-					if (data.info[i].state === 2) {
+					if (data.info[i].state == 2) {
 						$('.mark').eq(i).html('已退款').css({ 'color': '#FD7C7C' });
 					}
 				}
