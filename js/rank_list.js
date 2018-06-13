602cbc98f8de93e9af24582bb3118d362c58d6dd
kirito <1737466486@qq.com>
2018-06-13 14:03:58 +0800

    排行榜无效图片替换

--- js/rank_list.js
+++ js/rank_list.js
@@ -1,3 +1,5 @@
+"use strict";
+
 $(function () {
 	window.get_token = get_token;
 	var token = null;
@@ -11,32 +13,36 @@ $(function () {
 			//url: "../json/top.json",
 			async: true,
 			success: function success(data) {
-				console.log(data)
-				for(var i=0;i<data.info.top.length;i++){
+				console.log(data);
+				for (var i = 0; i < data.info.top.length; i++) {
 					//console.log(data.info.top[i])
-					if(i<3){
-						console.log($('.rank_ul_one .li'+i).html())
-						$('.rank_ul_one .li'+i).find('span').eq(0).find('img').attr('src','http://s.speaka.cn/' + data.info.top[i].head)
-					    $('.rank_ul_one .li'+i).find('span').eq(1).html(data.info.top[i].name)
-					    $('.rank_ul_one .li'+i).find('span').eq(2).find('em').html(data.info.top[i].score)
-					}else{
-						$('.rank_ul_two').append(`<li>
-						<b>No.${i+1}</b>
-						<span><img src="http://s.speaka.cn/${data.info.top[i].head}"/></span>
-						<span>${data.info.top[i].name}</span>
-						<span><em>${data.info.top[i].score}</em><img src="../img/宝石.png"/></span>
-					</li>`)
+					if (i < 3) {
+						console.log($('.rank_ul_one .li' + i).html());
+						$('.rank_ul_one .li' + i).find('span').eq(0).find('img').attr('src', 'http://s.speaka.cn/' + data.info.top[i].head);
+						$('.rank_ul_one .li' + i).find('span').eq(1).html(data.info.top[i].name);
+						$('.rank_ul_one .li' + i).find('span').eq(2).find('em').html(data.info.top[i].score);
+					} else {
+						var _head = null;
+						if (data.info.top[i].head == '') {
+							_head = '../img/mr.png';
+						} else {
+							_head = 'http://s.speaka.cn/' + data.info.top[i].head;
+						}
+						if (data.info.top[i].head.substr(5, 2) == 'mj') {
+							_head = '../img/mr.png';
+						}
+						$('.rank_ul_two').append("<li>\n\t\t\t\t\t\t<b>No." + (i + 1) + "</b>\n\t\t\t\t\t\t<span><img src=\"" + _head + "\"/></span>\n\t\t\t\t\t\t<span>" + data.info.top[i].name + "</span>\n\t\t\t\t\t\t<span><em>" + data.info.top[i].score + "</em><img src=\"../img/\u5B9D\u77F3.png\"/></span>\n\t\t\t\t\t</li>");
 					}
 				}
-				$('.rank_ul_three li b').html('No.'+data.info.mine.rank)
-				$('.rank_ul_three li').find('span').eq(0).find('img').attr('src','http://s.speaka.cn/' + data.info.mine.head)
-			    $('.rank_ul_three li').find('span').eq(1).html(data.info.mine.name)
-				$('.rank_ul_three li').find('span').eq(2).find('em').html(data.info.mine.score)
+				$('.rank_ul_three li b').html('No.' + data.info.mine.rank);
+				$('.rank_ul_three li').find('span').eq(0).find('img').attr('src', 'http://s.speaka.cn/' + data.info.mine.head);
+				$('.rank_ul_three li').find('span').eq(1).html(data.info.mine.name);
+				$('.rank_ul_three li').find('span').eq(2).find('em').html(data.info.mine.score);
 			},
-			error:function(res){
-				console.log(res)
+			error: function error(res) {
+				console.log(res);
 			}
 		});
 	}
-	console.log('1')
+	console.log('1');
 });
