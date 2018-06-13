4e166c5176caaca71f5a00686cafaaf70467fa9d
kirito <1737466486@qq.com>
2018-06-13 13:45:32 +0800

    排行榜

--- js/rank_list.js
+++ js/rank_list.js
@@ -1,14 +1,14 @@
 $(function () {
 	window.get_token = get_token;
 	var token = null;
-	get_token();
+	//get_token();
 	function get_token(_results) {
 		//console.log(_results)
 		token = 'Bearer ' + _results;
 		$.ajax({
 			type: "get",
-			//url: "http://api.speaka.cn/api/top100 ",
-			url: "../json/top.json",
+			url: "http://api.speaka.cn/api/top100 ",
+			//url: "../json/top.json",
 			async: true,
 			success: function success(data) {
 				console.log(data)
@@ -19,8 +19,19 @@ $(function () {
 						$('.rank_ul_one .li'+i).find('span').eq(0).find('img').attr('src','http://s.speaka.cn/' + data.info.top[i].head)
 					    $('.rank_ul_one .li'+i).find('span').eq(1).html(data.info.top[i].name)
 					    $('.rank_ul_one .li'+i).find('span').eq(2).find('em').html(data.info.top[i].score)
+					}else{
+						$('.rank_ul_two').append(`<li>
+						<b>No.${i+1}</b>
+						<span><img src="http://s.speaka.cn/${data.info.top[i].head}"/></span>
+						<span>${data.info.top[i].name}</span>
+						<span><em>${data.info.top[i].score}</em><img src="../img/宝石.png"/></span>
+					</li>`)
 					}
 				}
+				$('.rank_ul_three li b').html('No.'+data.info.mine.rank)
+				$('.rank_ul_three li').find('span').eq(0).find('img').attr('src','http://s.speaka.cn/' + data.info.mine.head)
+			    $('.rank_ul_three li').find('span').eq(1).html(data.info.mine.name)
+				$('.rank_ul_three li').find('span').eq(2).find('em').html(data.info.mine.score)
 			},
 			error:function(res){
 				console.log(res)
