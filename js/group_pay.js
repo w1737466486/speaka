d0a79f31d399bc8ce3fd0875cb0cb02a9a23e4a1
kirito <1737466486@qq.com>
2018-06-13 15:24:54 +0800

    Signed-off-by: kirito <1737466486@qq.com>

--- js/group_pay.js
+++ js/group_pay.js
@@ -106,7 +106,7 @@ $(function () {
 			if (data.group.length > 0) {
 				var group_member = $('.group_member li');
 				for (var i = 0; i < data.group.length; i++) {
-					$('.group_member li').eq(i).find('img').eq(1).attr('src', data.group[i].user_info.head_wx);
+					$('.group_member li').eq(i).find('img').eq(1).attr('src','http://s.speaka.cn/' +data.group[i].user_info.head);
 					$('.group_member li').eq(i).find('b').html(data.group[i].user_info.name);
 				}
 			}
