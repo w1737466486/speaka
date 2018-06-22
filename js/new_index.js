7b22ab2e948c2e40d913c80a84f11ac35d842fe8
kirito <1737466486@qq.com>
2018-06-22 10:21:37 +0800

    新首页提交

--- js/new_index.js
+++ js/new_index.js
@@ -1,5 +1,6 @@
 $(function () {
 	var word=null;
+	var word_id=null;
 	//默认页面点击搜索，显示搜索框，隐藏今日单词
 	$('.recommended_word_head p span').eq(1).click(function(){
 		$('.recommended_word_head').css({
@@ -9,10 +10,23 @@ $(function () {
 			'display':'block'
 		})
 	})
+	//每日一词推荐
+	$.ajax({
+		type:"get",
+		//url:'../json/day_word.json',
+		url:"http://api.speaka.cn/api/word/day-word",
+		async:false,
+		dataType:'JSON',
+		success:function(data){
+			$('.recommended_word_img').append('<img src="http://s.speaka.cn/'+data.info.word.pic_path+'">')
+			$('.recommended_word_box p').eq(0).html(data.info.word.eng)
+			$('.recommended_word_box p').eq(1).html(data.info.word.chn)
+			$('.recommended_word_box .mp3Btn').attr('src','http://s.speaka.cn/'+data.info.word.voice)	
+		}
+	});
 	//点击cancel或者search触发返回或搜索事件
 	$('.recommended_word_search p span').click(function(){
 		if($('.recommended_word_search p span img').attr('state')=='cancel'){
-		alert('2')
 		$('.recommended_word_search').css({
 			'display':'none'
 		})
@@ -20,22 +34,57 @@ $(function () {
 			'display':'block'
 		})
 		}else if($('.recommended_word_search p span img').attr('state')=='search'){
-			alert('1')
 				word=$('.recommended_word_search p input').val()
 				$.ajax({
 				type:"get",
 				dataType: 'JSON',
-				url:'../json/true_card.json',
-				//url:"http://api.speaka.cn/api/word/search?keyword="+word,
-				async:true,
+				//url:'../json/true_card.json',
+				//url:'../json/false_card.json',
+				url:"http://api.speaka.cn/api/word/search?keyword="+word,
+				async:false,
 				success:function(data){
-					alert(JSON.stringify(data))
+					//alert(JSON.stringify(data))
 					console.log(data)
 					$('.recommended_word_search p input').val('')
 					if(data.status==1){
-						
+						if(data.info[0].isCard){
+							console.log('命中闪卡')
+							word_id=data.info[0].id
+							console.log(word_id)
+							//命中闪卡
+							$.ajax({
+								type:"get",
+								//url:'../json/day_word.json',
+								//url:'../json/false_card.json',
+								url:"http://api.speaka.cn/api/word/day-word?id="+word_id,
+								async:false,
+								dataType:'JSON',
+								success:function(data){
+									alert('1234')
+									$('.recommended_word_img img').attr('src','http://s.speaka.cn/'+data.info.word.pic_path)
+									$('.recommended_word_box p').eq(0).html(data.info.word.eng)
+									$('.recommended_word_box p').eq(1).html(data.info.word.chn)
+									$('.recommended_word_box .mp3Btn').attr('src','http://s.speaka.cn/'+data.info.word.voice)
+								
+									
+								}
+							});
+							$('.recommended_word_word').hide()
+							$('.recommended_word_box').show()
+						}else{
+							$('.recommended_word_word').html('')
+							$('.recommended_word_word').append('<p>'+data.info[0].eng+'</p>')
+							var arr_word=data.info[0].chn.split("\\n")
+							console.log(arr_word)
+							for(var i=0;i<arr_word.length;i++){
+								$('.recommended_word_word').append('<p>'+arr_word[i]+'</p>')
+							}
+							
+							$('.recommended_word_word').show()
+							$('.recommended_word_box').hide()
+						}
 					}else{
-						
+						alert('查无此词，请重新输入！')
 					}
 					
 				},
@@ -46,6 +95,7 @@ $(function () {
 		}
 		
 	})
+	//键盘回车键触发搜索事件
 	$(".keyword").on('keypress',function(e) {  
         var keycode = e.keyCode;  
         var searchName = $(this).val();  
@@ -55,23 +105,55 @@ $(function () {
 				$.ajax({
 				type:"get",
 				dataType: 'JSON',
-				url:'../json/true_card.json',
-				//url:"http://api.speaka.cn/api/word/search?keyword="+searchName,
-				async:true,
+				//url:'../json/true_card.json',
+				//url:'../json/false_card.json',
+				url:"http://api.speaka.cn/api/word/search?keyword="+searchName,
+				async:false,
 				success:function(data){
 					//console.log(data)
 					$('.recommended_word_search p span img').attr('src','../img/search.png')
 					$('.recommended_word_search p span img').attr('state','search')
-					alert(JSON.stringify(data))
+					//alert(JSON.stringify(data))
 					$(".keyword").val('')
 					if(data.status==1){
 						if(data.info[0].isCard){
-							alert('123')
+							console.log('命中闪卡')
+							word_id=data.info[0].id
+							console.log(word_id)
+							//命中闪卡
+							$.ajax({
+								type:"get",
+								//url:'../json/day_word.json',
+								//url:'../json/false_card.json',
+								url:"http://api.speaka.cn/api/word/day-word?id="+word_id,
+								async:false,
+								dataType:'JSON',
+								success:function(data){
+									alert(JSON.stringify(data))
+									$('.recommended_word_img img').attr('src','http://s.speaka.cn/'+data.info.word.pic_path)
+									$('.recommended_word_box p').eq(0).html(data.info.word.eng)
+									$('.recommended_word_box p').eq(1).html(data.info.word.chn)
+									$('.recommended_word_box .mp3Btn').attr('src','http://s.speaka.cn/'+data.info.word.voice)
+								
+									
+								}
+							});
 							$('.recommended_word_word').hide()
 							$('.recommended_word_box').show()
+						}else{
+							$('.recommended_word_word').html('')
+							$('.recommended_word_word').append('<p>'+data.info[0].eng+'</p>')
+							var arr_word=data.info[0].chn.split("\\n")
+							console.log(arr_word)
+							for(var i=0;i<arr_word.length;i++){
+								$('.recommended_word_word').append('<p>'+arr_word[i]+'</p>')
+							}
+							
+							$('.recommended_word_word').show()
+							$('.recommended_word_box').hide()
 						}
 					}else{
-						
+						alert('查无此词，请重新输入！')
 					}
 				    console.log($(".keyword").val())	
 					
@@ -82,17 +164,12 @@ $(function () {
 			});
         }  
      }); 
-	
 	//音频播放
 	var audio = document.getElementById('mp3Btn');
-    audio.volume = 0.3;
+	audio.volume = 0.3;
 	$('.btn-audio').click(function() {
-            event.stopPropagation();//防止冒泡
-            if(audio.paused){ //如果当前是暂停状态
-                audio.play(); //播放
-                return;
-            }else{//当前是播放状态
-                audio.pause(); //暂停
-            }
-        });
+	      audio.play(); //播放
+	});
+								
+	
 })
