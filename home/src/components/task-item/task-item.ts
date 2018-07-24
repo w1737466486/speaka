d46d0d54018167641d99916c8ed437d4a327272a
matrixs <363986099@qq.com>
2018-07-24 11:43:47 +0800

    首页更新

--- home/src/components/task-item/task-item.ts
+++ home/src/components/task-item/task-item.ts
@@ -20,6 +20,7 @@ export class TaskItemComponent implements OnInit {
   isDone = false;
   type = -1;
   getTokenCallbackSignature = "receiveToken";
+  canGet = false;
 
   constructor(private http: HttpClient) {
   }
@@ -33,10 +34,12 @@ export class TaskItemComponent implements OnInit {
     const isFinish = this.item["isFinish"];
     const isGet = this.item["isGet"];
     this.isDone = true;
+    this.canGet = false;
     if (isGet != 0) {
       this.text = "已领取";
     } else if (isFinish >= num) {
-      this.text = "已完成";
+      this.text = "领取";
+      this.canGet = true;
     } else {
       this.text = "去完成";
       this.isDone = false;
@@ -53,10 +56,17 @@ export class TaskItemComponent implements OnInit {
     console.log(token);
     console.log(thx.type);
     const params = new HttpParams().set('type', `${thx.type}`);
-    thx.http.put("http://api.speaka.live/api/task/finishUserTask", {headers: headers, params: params})
-    .subscribe(data => {
-      console.log(data);
-    });
+    if (this.canGet) {
+      thx.http.put("http://api.speaka.live/api/task/getGem", {headers: headers})
+      .subscribe(data => {
+        console.log(data);
+      });
+    } else {
+      thx.http.put("http://api.speaka.live/api/task/finishUserTask", {headers: headers})
+      .subscribe(data => {
+        console.log(data);
+      });
+    }
   }
 
 }
