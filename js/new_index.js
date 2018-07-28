b78c1b49ea17fde48be30a707a65e550a4523047
kirito <1737466486@qq.com>
2018-07-28 19:06:17 +0800

    搜索页

--- js/new_index.js
+++ js/new_index.js
@@ -6,15 +6,7 @@ $(function () {
 	//var tokenurl = queryURL(location.href);
 	//var token='Bearer ' + tokenurl.token;
 	//get_token();
-	if (window.webkit) {
-		window.webkit.messageHandlers.getToken.postMessage('get_token');
-	} else {
-		//curson.punchCurson(JSON.stringify(obj));
-	}
-	function get_token(_results) {
-		//token = 'Bearer ' + 'b83eQAzanwJHD9WClsPva6iE7AcwdjMLs9QWlpjq';
-		token='Bearer ' + _results;
-	}
+	
 	//默认页面点击搜索，显示搜索框，隐藏今日单词
 	$('.recommended_word_head p span').eq(1).click(function(){
 		$('.recommended_word_head').css({
@@ -64,8 +56,15 @@ $(function () {
 					}
 				}
 			}*/
-			
-			$.ajax({
+			if (window.webkit) {
+				window.webkit.messageHandlers.getToken.postMessage('get_token');
+			} else {
+				//curson.punchCurson(JSON.stringify(obj));
+			}
+			function get_token(_results) {
+				//token = 'Bearer ' + 'b83eQAzanwJHD9WClsPva6iE7AcwdjMLs9QWlpjq';
+				token='Bearer ' + _results;
+				$.ajax({
 				type:"get",
 				url:"https://api.speaka.live/api/memberVideo/memberVideo",
 				//url:'../json/video_data.json',
@@ -115,6 +114,8 @@ $(function () {
 					
 				}
 			});
+			}
+			
 				
 		}
 	});
