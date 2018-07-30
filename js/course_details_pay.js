3e07a3302333609526470fe4d493983c494d6dbd
kirito <1737466486@qq.com>
2018-07-30 20:04:02 +0800

    Signed-off-by: kirito <1737466486@qq.com>

--- js/course_details_pay.js
+++ js/course_details_pay.js
@@ -139,7 +139,7 @@ $(function () {
 				//微信或App单人购,让团购价格消失
 				if (objurl.type_id == 11 || objurl.type_id == 21) {
 					pay_money = (data.price / 100 - Number(objurl.coupon_money));
-					pay_money>0?pay_money:pay_money=0.00
+					pay_money>0?pay_money:pay_money=0.01
 					$('.course_pay div').eq(1).find('span').html('实付： ￥' + pay_money.toFixed(2) + '元');
 					$('.course_pay p').eq(3).css({
 						'display': 'none'
@@ -148,7 +148,7 @@ $(function () {
 				//微信或App团购
 				if (objurl.type_id == 12 || objurl.type_id == 22) {
 					pay_group_money = (data.groupon_price / 100 - Number(objurl.coupon_money));
-					pay_group_money>0?pay_group_money:pay_group_money=0.00
+					pay_group_money>0?pay_group_money:pay_group_money=0.01
 					$('.course_pay div').eq(1).find('span').html('实付： ￥' + pay_group_money.toFixed(2) + '元');
 				}
 				$('.have').html("-￥" + objurl.coupon_money + "元");
