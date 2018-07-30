079d433fafc44b95595323843d8c9fd2891e4736
matrixs <363986099@qq.com>
2018-07-30 19:05:42 +0800

    查看全部二级页面

--- home/src/app/app.module.ts
+++ home/src/app/app.module.ts
@@ -4,31 +4,30 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { HomePage } from '../pages/home/home';
-import { TagVideoListPage } from '../pages/tag-video-list/tag-video-list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ComponentsModule } from '../components/components.module';
 
 import { HttpClientModule } from '@angular/common/http';
+import { TagVideoListPageModule } from '../pages/tag-video-list/tag-video-list.module';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    TagVideoListPage,
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
     ComponentsModule,
     HttpClientModule,
+    TagVideoListPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    TagVideoListPage,
   ],
   providers: [
     StatusBar,
