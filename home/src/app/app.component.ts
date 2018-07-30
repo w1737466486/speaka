b9c809e4ed9e465f505efbc89b9dea9259c3061f
matrixs <363986099@qq.com>
2018-07-30 18:19:08 +0800

    首页更新

--- home/src/app/app.component.ts
+++ home/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
+import { TagVideoListPage } from '../pages/tag-video-list/tag-video-list';
 
 @Component({
   templateUrl: 'app.html'
