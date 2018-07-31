dfd2955ed559797e56d3d6048ce5b1be1a7727d5
kirito <1737466486@qq.com>
2018-07-31 14:41:57 +0800

    Signed-off-by: kirito <1737466486@qq.com>

--- js/course_map.js
+++ js/course_map.js
@@ -24,20 +24,23 @@
 			}
 			return obj;
 		}
-
  	console.log(getNowFormatDate());
- 	
- 	var mapurl=location.href.split('?')[1];
- 	var url_map=null;
  	var team_id=null;
- 	if(mapurl){
- 		console.log(mapurl.substr(8));
- 		team_id=mapurl.substr(8);
- 	}else{
- 		team_id=77;
- 	}
- 	var obj={};
- 	$.ajax({
+ 	window.get_teamId=get_teamId;
+ 	if (window.webkit) {
+		window.webkit.messageHandlers.getData.postMessage('get_teamId');
+	} else {
+		//curson.punchCurson(JSON.stringify(obj));
+	}
+	//get_teamId();
+	function get_teamId(res){
+		team_id=res;
+		if(team_id){
+			team_id=team_id;
+		}else{
+			team_id=77;
+		}
+		$.ajax({
  		type:"get",
  		url:"https://api.speaka.live/api/team/"+team_id+"/commodity",
  		//url:"../json/speaka.json",
@@ -61,16 +64,9 @@
 	 			}
  			}
  		}
- 	});
-   /* $('.main .imgall').click(function(){
-    	//console.log($(this).attr('src'))
-    	$(this).attr('src','../img/'+$(this).index()+'.png')
-    })*/
-   /* $('.main .img1').click(function(){
-    	console.log(1)
-    	window.location.href = "../html/course.html?1";
-    }) */ 
-    
+ 	});	
+}
+ 
     $('.main .imgall').click(function(){
     	console.log($(this).attr('src').substr(7,6));
     	if($(this).attr('src').substr(7,6)=='locked'){
